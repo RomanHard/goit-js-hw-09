7bc2bd945666d5923a841de8e663049bd030a186
Roman <romeenice@gmail.com>
2023-01-06 00:16:01 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,30 +2,47 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-let isActive;
 const refs = {
+  dataPicker: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button[data-start]'),
-  dateDays: document.querySelector('[data-days]'),
-  dataHours: document.querySelector('[data-hours]'),
-  dataMinutes: document.querySelector('[data-minutes]'),
-  dataSeconds: document.querySelector('[data-seconds]'),
+  timerDays: document.querySelector('span[data-days]'),
+  timerHours: document.querySelector('span[data-hours]'),
+  timerMinutes: document.querySelector('span[data-minutes]'),
+  timerSeconds: document.querySelector('span[data-seconds]'),
 };
-// --- flatpickr
+let isActive;
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    hendleSelectedDate(selectedDate);
-  },
+function convertMs(timerTimeUTC) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(timerTimeUTC / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((timerTimeUTC % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(
+    Math.floor(((timerTimeUTC % day) % hour) / minute)
+  );
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((timerTimeUTC % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+const updateClockface = timerTimeUTC => {
+  const { days, hours, minutes, seconds } = convertMs(timerTimeUTC);
+  refs.timerDays.textContent = days;
+  refs.timerHours.textContent = hours;
+  refs.timerMinutes.textContent = minutes;
+  refs.timerSeconds.textContent = seconds;
+  console.log({ days, hours, minutes, seconds });
 };
-
-const fp = flatpickr('#datetime-picker', { options });
-
-/// - timer обробка
 
 const hendleSelectedDate = selectedDate => {
   let timerId = null;
@@ -57,33 +74,19 @@ const hendleSelectedDate = selectedDate => {
   }
 };
 
-function updateClockface({ days, hours, minutes, seconds }) {
-  refs.dateDays.textContent = `${days}`;
-  refs.dataHours.textContent = `${hours}`;
-  refs.dataMinutes.textContent = `${minutes}`;
-  refs.dataSeconds.textContent = `${seconds}`;
-}
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    hendleSelectedDate(selectedDate);
+  },
+};
 
-// ------- преоборазовуємо цифри 1 = 01.  5=05.
-function pad(value) {
+const fp = flatpickr('#datetime-picker', options);
+
+function addLeadingZero(value) {
   return String(value).padStart(2, '0');
-}
-
-// -------  Підрахунок мілісікунди в дні.часи.минути.секунди
-function convertMs(mtimerTimeUTCs) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = pad(Math.floor(ms / day));
-  // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-  return { days, hours, minutes, seconds };
 }
