2fb497f3b1699266c940871533efd3c978229b19
Roman <romeenice@gmail.com>
2023-01-06 00:21:14 +0200

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,3 +9,17 @@ let timerID = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+refs.start.addEventListener('click', () => {
+  refs.stop.removeAttribute('disabled');
+  refs.start.setAttribute('disabled', 'disabled');
+  TimerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+refs.stop.addEventListener('click', () => {
+  refs.stop.setAttribute('disabled', 'disabled');
+  refs.start.removeAttribute('disabled');
+  clearInterval(TimerId);
+});
