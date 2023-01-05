e2b5b6e03926cf1ffe5ec97a80b8c8df86968e0d
Roman <romeenice@gmail.com>
2023-01-06 00:17:40 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
