8bc1da9609ceb2c9363ea3423106ce58bc1ceafd
Roman <romeenice@gmail.com>
2023-01-05 20:14:51 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,1 +1,14 @@
-const date = Date.now();
+const timer = {
+  start() {
+    const startTime = Date.now();
+
+    setInterval(() => {
+      //   console.log('start -> startTime', startTime);
+      const currentTime = Date.now();
+
+      console.log(currentTime - startTime);
+    }, 1000);
+  },
+};
+
+timer.start();
