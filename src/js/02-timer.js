4da619f423d5742ba27bb58338abf286dd46f93a
Roman <romeenice@gmail.com>
2023-01-06 00:09:33 +0200

    fill

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,8 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
-
+let isActive;
 const refs = {
   startBtn: document.querySelector('button[data-start]'),
   dateDays: document.querySelector('[data-days]'),
@@ -18,31 +10,52 @@ const refs = {
   dataMinutes: document.querySelector('[data-minutes]'),
   dataSeconds: document.querySelector('[data-seconds]'),
 };
+// --- flatpickr
 
-const timer = {
-  isActive: false,
-  start() {
-    if (this.isActive) {
-      return;
-    }
-
-    const startTime = Date.now();
-    this.isActive = true;
-
-    setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const { days, hours, minutes, seconds } = convertMs(deltaTime);
-
-      updateClockface({ days, hours, minutes, seconds });
-      //   console.log(`${days}:${hours}:${minutes}:${seconds}`);
-    }, 1000);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    hendleSelectedDate(selectedDate);
   },
 };
 
-refs.startBtn.addEventListener('click', () => {
-  timer.start();
-});
+const fp = flatpickr('#datetime-picker', { options });
+
+/// - timer обробка
+
+const hendleSelectedDate = selectedDate => {
+  let timerId = null;
+
+  const selectedDateUTC = selectedDate.getTime();
+
+  const onStartBtnClick = () => {
+    if (isActive) {
+      return;
+    }
+    isActive = true;
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    timerId = setInterval(() => {
+      if (selectedDateUTC > Date.now()) {
+        const timerTimeUTC = selectedDateUTC - Date.now();
+        updateClockface(timerTimeUTC);
+      } else {
+        clearInterval(timerId);
+        isActive = false;
+      }
+    }, 1000);
+  };
+
+  if (Date.now() > selectedDateUTC) {
+    Notiflix.Notify.warning('Please choose a date in the future');
+  } else {
+    refs.startBtn.removeAttribute('disabled');
+    refs.startBtn.addEventListener('click', onStartBtnClick);
+  }
+};
 
 function updateClockface({ days, hours, minutes, seconds }) {
   refs.dateDays.textContent = `${days}`;
@@ -57,7 +70,7 @@ function pad(value) {
 }
 
 // -------  Підрахунок мілісікунди в дні.часи.минути.секунди
-function convertMs(ms) {
+function convertMs(mtimerTimeUTCs) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
