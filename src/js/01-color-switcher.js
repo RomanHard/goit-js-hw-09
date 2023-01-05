312610442adb1275f50e46dcbbe1eac1e3bf7976
Roman <romeenice@gmail.com>
2023-01-06 00:18:46 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
 const refs = {
   body: document.querySelector('body'),
   start: document.querySelector('button[data-start]'),
   stop: document.querySelector('button[data-stop]'),
 };
+
+let timerID = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
