06c8fde5d13c65a92fa50d25263e3f7b75c232dc
RomanHard <86237018+RomanHard@users.noreply.github.com>
2023-01-10 21:46:27 +0000

    Deploying to gh-pages from @ RomanHard/goit-js-hw-09@0ba03a5a13ffa49cd31d4a0c07af6a5e0e7ea14f 🚀

--- 01-color-switcher.25187119.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};t.start.addEventListener("click",(()=>{t.stop.removeAttribute("disabled"),t.start.setAttribute("disabled","disabled"),timeId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(()=>{t.stop.setAttribute("disabled","disabled"),t.start.removeAttribute("disabled"),clearInterval(timeId)}));
-//# sourceMappingURL=01-color-switcher.25187119.js.map
